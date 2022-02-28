d8897e34239002a85da042e3416a13d5ba82a4c0
Daniel Carvalho <danielborgesdecarvalho@gmail.com>
2022-02-28 14:19:01 -0300

    feat: add create user unity test

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,39 @@
+import { AppError } from "../../../../shared/errors/AppError"
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository"
+import { CreateUserUseCase } from "./CreateUserUseCase"
+
+let createUserUseCase: CreateUserUseCase
+let userRepositoryInMemory: InMemoryUsersRepository
+
+describe('Create User', () => {
+  beforeEach(() => {
+    userRepositoryInMemory = new InMemoryUsersRepository()
+    createUserUseCase = new CreateUserUseCase(userRepositoryInMemory)
+  })
+
+  it('should be able to create a new user', async () => {
+    const newUser = {
+      name: 'John',
+      email: 'john@example.com',
+      password: 'password'
+    }
+
+    const response = await createUserUseCase.execute(newUser)
+    console.log("🚀 ~ file: CreateUserUseCase.spec.ts ~ line 21 ~ it ~ response", response)
+
+    expect(response).toHaveProperty('id')
+  })
+
+  it('should be not be able to create a new user with existent email', async () => {
+    expect(async () => {
+      const newUser = {
+        name: 'John',
+        email: 'john@example.com',
+        password: 'password'
+      }
+  
+     await createUserUseCase.execute(newUser)
+     await createUserUseCase.execute(newUser)
+    }).rejects.toBeInstanceOf(AppError)
+  })
+})
